025c31a4a41cbb3b1510936c3a565a4d20a972cc
Honzoraptor <j.ackermann@email.cz>
2024-07-23 08:57:37 +0200

    Made UI that properly sticks to the screen, made a better floor texture and some polishing

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,7 @@
 extends Node2D
 
-
-@onready var player = $Player
-
 func _ready():
 	pass
 
-func _process(delta):
+func _process(_delta):
 	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,7 +6,7 @@ func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
 	look_at(get_global_mouse_position())
